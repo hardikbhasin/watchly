f778c3101e651ea132ff5461bbb42e9e88ce33d0
hardikbhasin <hardikbhasin398@gmail.com>
2023-08-24 15:17:00 +0530

    started feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import  Sidebar  from './Sidebar';
 
+// import { fetchFromAPI } from '../utils/fetchFromAPI';
 const Feed = () => {
   return (
-    <div>
-      Feed
-    </div>
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          Copyright 2023 Watchly
+        </Typography>
+      </Box>
+      {/* <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          {selectedCategry} <span style={{ color: '#F31503' }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box> */}
+    </Stack>
   )
 }
 
