588011b2371de3a2b4ba6e76c9e4552cdee3383a
hardikbhasin <hardikbhasin398@gmail.com>
2023-08-26 19:19:25 +0530

    fixed the data

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,10 +7,10 @@ import Videos from './Videos';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
-
-
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+    setVideos(data.items));
   }, [selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
@@ -42,7 +42,7 @@ const Feed = () => {
         >
           {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
 
     </Stack>
