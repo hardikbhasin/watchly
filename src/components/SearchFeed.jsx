79bcec30295527ba21dc3f244548aabe21c93830
hardikbhasin <hardikbhasin398@gmail.com>
2023-08-30 23:21:06 +0530

    searchFeed initilized

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
+import Videos from './Videos';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 const SearchFeed = () => {
+  
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=`).then((data) =>
+    setVideos(data.items));
+  }, []);
   return (
-    <div>
-      
-    </div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+         Seach results for: <span style={{ color: '#F31503' }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
   )
 }
 
