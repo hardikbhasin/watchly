428f3c05e2e8f1d4ba25826bee1ae060bbf9c2e2
hardikbhasin <hardikbhasin398@gmail.com>
2023-08-30 23:13:02 +0530

    channelcard fixed marginTop

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -5,7 +5,7 @@ import { CheckCircle } from '@mui/icons-material';
 import { demoProfilePicture } from '../utils/constants';
 import { demoChannelTitle } from '../utils/constants';
 
-const ChannelCard = ({ channelDetail }) => (
+const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
     sx={{
       boxShadow: 'none',
@@ -16,7 +16,7 @@ const ChannelCard = ({ channelDetail }) => (
       width: { sx: '356px', md: '320px' },
       height: '326px',
       margin: 'auto',
-      // marginTop,
+      marginTop,
     }}
   >
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
